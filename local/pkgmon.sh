09db013c73317916d7bbcac8f44e838d96b60333
Alexander Kozlov <akozlov@nada.kth.se>
2015-02-28 11:37:24 +0100

    pkgmon

---
+++ local/pkgmon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+P=$(pwd)
+cd /tmp
+rm -f .listing* index.html*
+echo Using mirrors:
+for m in ftp://trumpetti.atm.tut.fi/gentoo/distfiles ftp://ftp.kernel.org/pub/linux/kernel/v3.x; do
+  echo $m
+  wget -q --no-remove-listing $m/
+  awk '{ print $9 }' .listing >> .listing-tmp
+done
+echo
+sort -u .listing-tmp > .listing-files
+if [ -d $P/ports ]; then
+  for p in $P/ports/*; do
+    source $p/Pkgfile
+    echo $name $version-$release:
+    grep "^$name" .listing-files
+    echo
+  done
+fi
+rm -f .listing* index.html
