fadb88049835a12725fc585c99695da0ee308f70
Alexander Kozlov <akozlov@kth.se>
2021-09-22 09:53:18 +0200

    20210922

--- local/crux-ports.sync.sh
+++ local/crux-ports.sync.sh
@@ -5,7 +5,7 @@ echo -n sync $ver ports: ''
 for p in core opt xorg contrib; do
   ( echo -n $p ''
     rsync -aqz --del crux.nu::ports/crux-$ver/$p/ $p > /dev/null 2>&1 &&
-    find $p \( -name COPY* -o -name contrib.pub \) -delete #&&
+    find $p \( -name COPY* -o -name contrib.pub \) -delete &&
     find $p -name Pkgfile | xargs sed -i 's/pkginfo -i/pkz -i list/'
   ) || echo -n FAIL ''
 done
