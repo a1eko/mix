ed2b8145f929c2a0c35a1b1907558115cd73bc8a
Alexander Kozlov <akozlov@kth.se>
2025-08-09 11:47:49 +0200

    20250809

--- local/pkgmon.sh
+++ local/pkgmon.sh
@@ -6,42 +6,49 @@ S=/var/log/sources/pkgmon.log
 T=/tmp/pkgmon.log
 P=$(pwd)
 
+dist_files() {
+  local url=$1
+  wget -qO- $url | fmt -w1 | grep href= | grep -v http \
+    | grep -e "\.tar\." -e zip -e patch -e diff | cut -d'"' -f2 \
+    | grep -v -e '.asc$' -e '.sign$' -e '.sig$'
+}
+
 monitor() {
   cd /tmp
   rm -f .listing* index.html* distfiles-list
+
   echo -n "monitoring "
+
   for m in ${M[@]}; do
     echo -n "$m... "
     wget -qO- $m | fmt -w1 | grep href= | cut -d'"' -f2 >>.listing-tmp
   done
+
   pypi=https://pypi.python.org/
   echo -n "$pypi... "
   wget -qO- $pypi/pypi?%3Aaction=index \
     | grep 'href="/pypi/' | cut -d'"' -f2 \
     | sed -e 's,/pypi/,,'i -e 's,/,-,' \
       >>.listing-tmp
+
   lfs=http://www.linuxfromscratch.org/
   echo -n "$lfs... "
   if [ ! -e /tmp/wget-list ]; then
     wget -q --timeout=5 $lfs/lfs/downloads/development/wget-list || (echo no /tmp/wget-list && exit)
   fi
+
   dist=https://distfiles.gentoo.org/distfiles/
   echo -n "$dist..."
   dirs=$(wget -qO- $dist | fmt -w1 | grep href= | grep -v https | cut -d'"' -f2 | grep -E '^../')
   for d in $dirs; do
-    echo -n "."
-    files=$(wget -qO- $dist/$d | fmt -w1 | grep href= | grep -v http \
-      | grep -e "\.tar\." -e zip -e patch -e diff | cut -d'"' -f2 \
-      | grep -v -e '.asc$' -e '.sign$' -e '.sig$')
-    for f in $files; do
-      echo $dist/$d$f >>distfiles-list
-    done
-  done
-  for f in $(cat wget-list distfiles-list); do
-    basename $f >>.listing-tmp
-  done
+    dist_files $dist/$d &
+  done >>distfiles-list
+  wait
+
+  basename -a $(cat wget-list distfiles-list) >>.listing-tmp
   sort -u .listing-tmp >.listing-files
   echo " done"
+
   if [ -d $P/ports ]; then
     for p in $P/ports/*; do
       if [ -e $p/Pkgfile ]; then
@@ -52,7 +59,7 @@ monitor() {
       fi
     done >$T
   fi
-  #rm -f .listing* index.html*
+  rm -f .listing* index.html*
   touch $S
   comm -13 <(sort $S) <(sort $T)
 }
